90fe0d3617203466f2d87d9e69d32953f5549fb8
Khusi Jain <Khushijain040502@gmail.com>
2025-10-03 18:45:51 +0530

    update files and add more files

--- app.cjs
+++ app.cjs
@@ -1,14 +1,21 @@
+const ejsMate = require("ejs-mate");
+
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const path = require("path");
 const app = express("");
 const Listing = require("./models/listing.cjs");
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
+
 // connect to mongodb
 
 main()
@@ -59,6 +66,27 @@ app.post("/listings", async (req, res) => {
 app.get("/listings/:id", async (req, res) => {
   const listing = await Listing.findById(req.params.id);
   res.render("./listings/show.ejs", { listing });
+});
+
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("./listings/edit.ejs", { listing });
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  console.log(id);
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+  console.log("updated");
+});
+
+app.delete("/listings/:id", async (req, res) => {
+  await Listing.findByIdAndDelete(req.params.id, { deleted: true });
+  res.redirect("/listings");
 });
 
 app.get("/", (req, res) => {
